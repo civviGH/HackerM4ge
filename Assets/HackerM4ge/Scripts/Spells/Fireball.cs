c7d4c43a3196922dc8a380ec866c87eecfda8989
Fussl <s6chvoss@uni-bonn.de>
2017-02-17 23:01:42 +0100

    initial commit

---
+++ Assets/HackerM4ge/Scripts/Spells/Fireball.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fireball : MonoBehaviour, Spell {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/HackerM4ge/Scripts/WandController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WandController : MonoBehaviour {
+
+
+	// buttons
+	private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+	private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+	private Valve.VR.EVRButtonId menuButton = Valve.VR.EVRButtonId.k_EButton_ApplicationMenu;
+	private Valve.VR.EVRButtonId touchpad = Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad;
+	
+	// controller initalize
+	private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index);}}
+	private SteamVR_TrackedObject trackedObj;
+  
+  
+	// Use this for initialization
+	void Start () {
+    trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
