2abe335ada1662559484c1d5da610fb835469ba9
Fussl <s6chvoss@uni-bonn.de>
2017-02-18 20:31:07 +0100

    changes the camera rig to be a prefab. fixes the construct scene

--- Assets/HackerM4ge/Scripts/Spells/MeteorSpell.cs
+++ Assets/HackerM4ge/Scripts/Spells/MeteorSpell.cs
@@ -3,79 +3,80 @@ using System.Collections;
 
 public class MeteorSpell : Spell {
 
-	public Material Thumbnail;
+  public Material Thumbnail;
 
-	private GameObject previewSpherePrefab;
-	private GameObject previewSphere;
-	private GameObject meteorPrefab;
-	private LayerMask raycastLayerMask;
+  private GameObject previewSpherePrefab;
+  private GameObject previewSphere;
+  private GameObject meteorPrefab;
+  private LayerMask raycastLayerMask;
 
-	public MeteorSpell(){
-		this.Thumbnail = Resources.Load ("MeteorThumbnailMaterial") as Material;
-		this.previewSpherePrefab = Resources.Load ("MeteorSpellPrefabs/MeteorPreviewPrefab") as GameObject;
-		this.raycastLayerMask = LayerMask.GetMask ("Surfaces");
-		this.meteorPrefab = Resources.Load ("MeteorSpellPrefabs/Meteor2") as GameObject;
-	}
+  public MeteorSpell(){
+    this.Thumbnail = Resources.Load ("MeteorThumbnailMaterial") as Material;
+    this.previewSpherePrefab = Resources.Load ("MeteorSpellPrefabs/MeteorPreviewPrefab") as GameObject;
+    this.raycastLayerMask = LayerMask.GetMask ("Surfaces");
+    this.meteorPrefab = Resources.Load ("MeteorSpellPrefabs/Meteor2") as GameObject;
+  }
 
-	public void UpdateSpell(
-		TriggerState triggerState, 
-		Vector2 touchpadAxis, 
-		Vector3 wandPosition,
-		Vector3 wandDirection){
+  public void UpdateSpell(
+    TriggerState triggerState, 
+    Vector2 touchpadAxis, 
+    Vector3 wandPosition,
+    Vector3 wandDirection){
+    if (triggerState.press) {
+      RightTriggerPress (wandPosition, wandDirection);
+    }
+    if (triggerState.up) {
+      RightTriggerUp ();
+    }
+  }
 
-		if (triggerState.press)
-			RightTriggerPress (wandPosition, wandDirection);
-		if (triggerState.up)
-			RightTriggerUp ();
-	}
+  private void RightTriggerDown(){
+  }
 
-	private void RightTriggerDown(){
-	}
+  private void RightTriggerPress(Vector3 wandPosition, Vector3 wandDirection){
+    RaycastHit hitObject;
+    Ray ray = new Ray (wandPosition, wandDirection);
+    if (Physics.Raycast (ray, out hitObject, Mathf.Infinity, this.raycastLayerMask)) {
+      if (this.previewSphere)
+        this.previewSphere.transform.position = hitObject.point;
+      else {
+        this.previewSphere = GameObject.Instantiate (this.previewSpherePrefab, hitObject.point, new Quaternion ());
+      }
+    } else {
+      if (this.previewSphere)
+        UnityEngine.Object.Destroy (this.previewSphere);
+    }
+  }
 
-	private void RightTriggerPress(Vector3 wandPosition, Vector3 wandDirection){
-		RaycastHit hitObject;
-		Ray ray = new Ray (wandPosition, wandDirection);
-		if (Physics.Raycast (ray, out hitObject, Mathf.Infinity, this.raycastLayerMask)) {
-			if (this.previewSphere)
-				this.previewSphere.transform.position = hitObject.point;
-			else {
-				this.previewSphere = GameObject.Instantiate (this.previewSpherePrefab, hitObject.point, new Quaternion ());
-			}
-		} else {
-			if (this.previewSphere)
-				UnityEngine.Object.Destroy (this.previewSphere);
-		}
-	}
+  private void RightTriggerUp(){
+    if (this.previewSphere) {
+      SpawnMeteorToPosition (previewSphere.transform.position);	
+      UnityEngine.Object.Destroy (this.previewSphere);
+    }
+  }
 
-	private void RightTriggerUp(){
-		if (this.previewSphere) {
-			SpawnMeteorToPosition (previewSphere.transform.position);	
-			UnityEngine.Object.Destroy (this.previewSphere);
-		}
-	}
+  private void SpawnMeteorToPosition(Vector3 targetArea){
+    // instantiate meteor
+    GameObject actualMeteor = GameObject.Instantiate (
+      this.meteorPrefab, targetArea + new Vector3 (
+        Random.Range (-15f, 15f),
+        20,
+        Random.Range (-15f, 15f)),
+      new Quaternion ());
+    actualMeteor.GetComponent<MeteorController> ().StartFalling (targetArea);
+    actualMeteor.GetComponent<AudioSource> ().Play ();
+  }
 
-	private void SpawnMeteorToPosition(Vector3 targetArea){
-		// instantiate meteor
-		GameObject actualMeteor = GameObject.Instantiate (
-			                          this.meteorPrefab, targetArea + new Vector3 (
-				                          Random.Range (-15f, 15f),
-				                          20,
-				                          Random.Range (-15f, 15f)),
-			                          new Quaternion ());
-		actualMeteor.GetComponent<MeteorController> ().StartFalling (targetArea);
-		actualMeteor.GetComponent<AudioSource> ().Play ();
-	}
-	
-	public Material GetThumbnail(){
-		return this.Thumbnail;
-	}
+  public Material GetThumbnail(){
+    return this.Thumbnail;
+  }
 
-	public string GetName(){
-		return "Meteor";
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+  public string GetName(){
+    return "Meteor";
+  }
+
+  // Update is called once per frame
+  void Update () {
+    
+  }
 }
