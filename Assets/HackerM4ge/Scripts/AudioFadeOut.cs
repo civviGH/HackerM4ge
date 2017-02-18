e6f0157c2b6548fbf2a044056a55b9182b335234
Fussl <s6chvoss@uni-bonn.de>
2017-02-18 21:25:22 +0100

    audiofadeout static and making fire sound of meteor a child

---
+++ Assets/HackerM4ge/Scripts/AudioFadeOut.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class AudioFadeOut {
+
+    public static IEnumerator FadeOut (AudioSource audioSource, float fadeTime, GameObject objectToDestroy = null) {
+        float startVolume = audioSource.volume;
+        Debug.Log (startVolume);
+        while (audioSource.volume > 0f) {
+            audioSource.volume -= startVolume * 1f/60f / fadeTime;
+            Debug.Log (audioSource.volume);
+            yield return new WaitForSeconds(1f/60f);
+        }
+        Debug.Log (audioSource.volume);
+        audioSource.Stop ();
+        if (objectToDestroy != null) {
+            UnityEngine.Object.Destroy (objectToDestroy);
+        }
+    }
+}
