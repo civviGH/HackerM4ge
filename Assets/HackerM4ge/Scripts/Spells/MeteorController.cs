293082a8ff2721827b5db6f918770940c6804d4a
Fussl <s6chvoss@uni-bonn.de>
2017-02-18 22:24:13 +0100

    meteor best spell EUW. audiofadeout has option to destroy given object after coroutine finishes

--- Assets/HackerM4ge/Scripts/Spells/MeteorController.cs
+++ Assets/HackerM4ge/Scripts/Spells/MeteorController.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class MeteorController : MonoBehaviour {
 
-  private Vector3 direction;
+    private Vector3 direction;
 
-  public GameObject explosionSoundObject;
-  public GameObject fireSoundObject;
+    public GameObject explosionSoundObject;
+    public GameObject fireSoundObject;
+    public GameObject explosionParticleObject;
+    public GameObject fireShrapnelObject;
 
-  public MeteorController(){
-  }
+    public MeteorController(){
+    }
 
   public void StartFalling(Vector3 targetPosition){
     this.direction = targetPosition - this.transform.position;
@@ -37,7 +39,17 @@ public class MeteorController : MonoBehaviour {
       explosionSoundObject.transform.parent = null;
       explosionSoundObject.GetComponent<AudioSource> ().Play ();
 
+      // explosion particles
+            explosionParticleObject.transform.parent = null;
+            explosionParticleObject.GetComponent<ParticleSystem> ().Play();
+
+            // shrapnels
+            fireShrapnelObject.transform.parent = null;
+            fireShrapnelObject.GetComponent<ParticleSystem> ().Play();
+
       // destroy objects
+            Destroy(explosionParticleObject, 1f);
+            Destroy (fireShrapnelObject, 1f);
       Destroy (fireSoundObject, 1.5f);
       Destroy (explosionSoundObject, 2f);
       Destroy (gameObject);
