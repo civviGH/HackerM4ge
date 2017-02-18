ac4eff8063ed69e0b3782c5ae6841eeb57fd9c73
Fussl <s6chvoss@uni-bonn.de>
2017-02-18 14:56:35 +0100

    adds buttonstate and triggerstate classes

---
+++ Assets/HackerM4ge/Scripts/ButtonState.cs
@@ -0,0 +1,11 @@
+﻿public class ButtonState {
+	public bool up;
+	public bool down;
+	public bool press;
+
+	public ButtonState(bool up, bool down, bool press){
+		this.up = up;
+		this.down = down;
+		this.press = press;
+	}
+}
