6966e0008a8d1e3f160e024602cc79d4a2665818
Fussl <s6chvoss@uni-bonn.de>
2017-02-18 12:47:15 +0100

    updates the spell interface

--- Assets/HackerM4ge/Scripts/Spell.cs
+++ Assets/HackerM4ge/Scripts/Spell.cs
@@ -1,3 +1,10 @@
-﻿public interface Spell {
+﻿using UnityEngine;
 
+public interface Spell {
+
+	Material GetThumbnail ();
+	string GetName();
+	void ButtonUp();
+	void ButtonDown();
+	void ButtonPress();
 }
