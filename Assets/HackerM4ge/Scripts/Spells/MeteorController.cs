76b095d1b5aeb662dc8c52576098067b98db7676
Fussl <s6chvoss@uni-bonn.de>
2017-02-18 16:55:41 +0100

    implements meteor spell. visuals not final

---
+++ Assets/HackerM4ge/Scripts/Spells/MeteorController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorController : MonoBehaviour {
+
+	private Vector3 startPosition;
+	private Vector3 targetPosition;
+
+	private float speed;
+	private float startTime;
+	private float wayToTravel;
+
+	public MeteorController(){
+	}
+
+	public void StartFalling(Vector3 targetPosition, float speed = 2f){
+		this.targetPosition = targetPosition;
+		this.speed = speed;
+	}
+
+	// Use this for initialization
+	void Start () {
+		this.startTime = Time.time;
+		this.startPosition = this.transform.position;
+		this.wayToTravel = Vector3.Distance (this.transform.position, this.targetPosition);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float distCovered = (Time.time - this.startTime) * this.speed;
+		float fracJourney = distCovered / this.wayToTravel;
+		this.transform.position = Vector3.Lerp (this.startPosition, targetPosition, fracJourney);
+	}
+
+	void OnTriggerEnter(){
+		Destroy (gameObject, 0.1f);
+	}
+}
--- Assets/HackerM4ge/Scripts/Spells/MeteorSpell.cs
+++ Assets/HackerM4ge/Scripts/Spells/MeteorSpell.cs
@@ -7,10 +7,14 @@ public class MeteorSpell : Spell {
 
 	private GameObject previewSpherePrefab;
 	private GameObject previewSphere;
+	private GameObject meteorPrefab;
+	private LayerMask raycastLayerMask;
 
 	public MeteorSpell(){
 		this.Thumbnail = Resources.Load ("MeteorThumbnailMaterial") as Material;
 		this.previewSpherePrefab = Resources.Load ("MeteorSpellPrefabs/MeteorPreviewPrefab") as GameObject;
+		this.raycastLayerMask = LayerMask.GetMask ("Surfaces");
+		this.meteorPrefab = Resources.Load ("MeteorSpellPrefabs/Meteor") as GameObject;
 	}
 
 	public void UpdateSpell(
@@ -19,36 +23,48 @@ public class MeteorSpell : Spell {
 		Vector3 wandPosition,
 		Vector3 wandDirection){
 
-		if (triggerState.up)
-			RightTriggerUp ();
-		if (triggerState.down)
-			RightTriggerDown (wandPosition, wandDirection);
 		if (triggerState.press)
 			RightTriggerPress (wandPosition, wandDirection);
+		if (triggerState.up)
+			RightTriggerUp ();
 	}
 
-	private void RightTriggerDown(Vector3 wandPosition, Vector3 wandDirection){
-		RaycastHit hitObject;
-		Ray ray = new Ray (wandPosition, wandDirection);
-		if (Physics.Raycast (ray, out hitObject)) {
-			this.previewSphere = GameObject.Instantiate(this.previewSpherePrefab, hitObject.point, new Quaternion (0f, 0f, 0f, 0f)) as GameObject;
-		}
+	private void RightTriggerDown(){
 	}
 
 	private void RightTriggerPress(Vector3 wandPosition, Vector3 wandDirection){
 		RaycastHit hitObject;
-		LayerMask raycastLayerMask = ~(1 << 2);
 		Ray ray = new Ray (wandPosition, wandDirection);
-		if (Physics.Raycast (ray, out hitObject, raycastLayerMask)) {
-			Debug.Log (hitObject.point);
-			this.previewSphere.transform.position = hitObject.point;
+		if (Physics.Raycast (ray, out hitObject, Mathf.Infinity, this.raycastLayerMask)) {
+			if (this.previewSphere)
+				this.previewSphere.transform.position = hitObject.point;
+			else {
+				this.previewSphere = GameObject.Instantiate (this.previewSpherePrefab, hitObject.point, new Quaternion ());
+			}
+		} else {
+			if (this.previewSphere)
+				UnityEngine.Object.Destroy (this.previewSphere);
 		}
 	}
 
 	private void RightTriggerUp(){
-		UnityEngine.Object.Destroy (this.previewSphere);
+		if (this.previewSphere) {
+			SpawnMeteorToPosition (previewSphere.transform.position);	
+			UnityEngine.Object.Destroy (this.previewSphere);
+		}
 	}
 
+	private void SpawnMeteorToPosition(Vector3 targetArea){
+		// instantiate meteor
+		GameObject actualMeteor = GameObject.Instantiate (
+			                          this.meteorPrefab, targetArea + new Vector3 (
+				                          Random.Range (-5f, 5f),
+				                          20,
+				                          Random.Range (-5f, 5f)),
+			                          new Quaternion ());
+		actualMeteor.GetComponent<MeteorController> ().StartFalling (targetArea, 20f);
+	}
+	
 	public Material GetThumbnail(){
 		return this.Thumbnail;
 	}
