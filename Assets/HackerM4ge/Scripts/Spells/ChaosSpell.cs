290f3ec40000d0af44e4cdbfdd6221e62869a2e7
Lars Wallenborn <lars@wallenborn.net>
2017-02-18 23:44:44 +0100

    adds Chaos spell

---
+++ Assets/HackerM4ge/Scripts/Spells/ChaosSpell.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+public class ChaosSpell : MonoBehaviour, Spell {
+    private Material material;
+
+    public ChaosSpell()
+    {
+        material = Resources.Load("ChaosSpellThumbnailMaterial", typeof(Material)) as Material; 
+    }
+
+    public string GetName()
+    {
+        return "Chaos";
+    }
+
+    public Material GetThumbnail()
+    {
+        return material;
+    }
+
+    public void UpdateSpell(TriggerState triggerState, Vector2 touchpadAxis, Vector3 wandPosition, Vector3 wandDirection)
+    {
+        Debug.Log(wandPosition);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
