1db7ae619ca87ee13691c57f1d71c3b3a9eb9fc6
Fussl <s6chvoss@uni-bonn.de>
2017-02-19 02:12:14 +0100

    ads health manager and homenetwork script

---
+++ Assets/HackerM4ge/Scripts/HealthManagerScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManagerScript : MonoBehaviour {
+
+    private Material skyboxMaterial;
+    private Color originalSkyboxColor = new Color(0.73f, 0.73f, 0.73f, 1f);
+    private List<GameObject> homeNetworks = new List<GameObject>();
+
+    // Use this for initialization
+    void Start () {
+        // main camera skybox
+        this.skyboxMaterial = GameObject.Find ("Camera (eye)").GetComponent<Skybox>().material;
+        skyboxMaterial.SetColor("_Tint", this.originalSkyboxColor);
+    }
+	
+    // Update is called once per frame
+    void Update () {
+        
+    }
+
+    public void RegisterNetwork(GameObject network){
+        this.homeNetworks.Add (network);
+    }
+
+    public void LeaveNetwork(GameObject network){
+        this.homeNetworks.Remove (network);
+        if (this.homeNetworks.Count == 0) {
+            Debug.Log ("GAMEOVER BITCHES");
+        }
+    }
+
+    public void DamageFlash(){
+        StartCoroutine (SkyboxColorFlash ());
+    }
+
+    IEnumerator SkyboxColorFlash(){
+        this.skyboxMaterial.SetColor ("_Tint", new Color (1f, 0f, 0f, 1f));
+        yield return new WaitForSeconds (0.1f);
+        this.skyboxMaterial.SetColor ("_Tint", this.originalSkyboxColor);
+    }
+}
---
+++ Assets/HackerM4ge/Scripts/HomeNetworkScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeNetworkScript : MonoBehaviour
+{
+    private HealthManagerScript healthManager;
+    private AudioSource damageSoundSource;
+
+    private int health = 50;
+
+    // Use this for initialization
+    void Start ()
+    {
+        this.damageSoundSource = gameObject.GetComponent<AudioSource> ();
+
+        this.healthManager = GameObject.Find ("HealthManager").GetComponent<HealthManagerScript> ();
+        this.healthManager.RegisterNetwork (gameObject);
+    }
+	
+    // Update is called once per frame
+    void Update ()
+    {
+		
+    }
+
+    public void Damage(int damage)
+    {
+        this.health -= damage;
+        // Debug.Log ("Network health: " + this.health);
+        if (this.health > 0) {
+            PlayDamageSound ();
+            this.healthManager.DamageFlash ();
+            return;
+        }
+        PlayDeathSound ();
+        this.healthManager.LeaveNetwork (gameObject);
+        Destroy (gameObject, 1f);
+    }
+
+    private void PlayDamageSound(){
+        this.damageSoundSource.Play ();
+    }
+
+    private void PlayDeathSound(){
+        // TODO
+    }
+}
