30740be604b29713ceed2c77a77597603ccfac13
Tobias Gödderz <tobias@goedderz.info>
2017-02-23 13:15:14 +0100

    Added a (mostly empty) TeleportController script

---
+++ Assets/HackerM4ge/Scripts/TeleportController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using SteamVRHelper;
+
+public class TeleportController : MonoBehaviour
+{
+    private SteamVR_TrackedObject trackedObj;
+    private ControllerType controllerType;
+
+    void Start()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+
+        controllerType = Helper.GetControllerType(Controller());
+    }
+
+    void Update()
+    {
+
+    }
+
+    private SteamVR_Controller.Device Controller()
+    {
+        return SteamVR_Controller.Input((int)trackedObj.index);
+    }
+}
