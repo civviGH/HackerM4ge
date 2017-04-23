7c3d9727358def09b8cebb1480b96a6e6d15e6a2
Lars Wallenborn <lars@wallenborn.net>
2017-04-23 13:01:18 +0200

    flickering key prototype

---
+++ Assets/HackerM4ge/Scripts/KeySummonerSerial.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeySummonerSerial : MonoBehaviour {
+
+    public float minDistance = 2;
+    public bool showing = false;
+
+    private Animator[] children;
+
+	// Use this for initialization
+	void Start () {
+        children = GetComponentsInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 delta = Camera.main.transform.position - transform.position;
+        if (delta.magnitude < minDistance)
+        {
+            if (showing) return;
+            showing = true;
+        }
+        else
+        {
+            if (!showing) return;
+            showing = false;
+        }
+        animateKeys(showing);
+    }
+
+    private void animateKeys(bool state)
+    {
+        for(int i = 0; i < children.Length; i++)
+        {
+            children[i].SetBool("Shown", state);
+        }
+    }
+}
