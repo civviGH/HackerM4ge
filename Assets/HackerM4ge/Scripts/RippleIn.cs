92b91a80c2535c6348545b0f8e96cb5438929d51
Lars Wallenborn <lars@wallenborn.net>
2017-08-26 15:58:30 +0200

    better animation, randomize highlights

--- Assets/HackerM4ge/Scripts/RippleIn.cs
+++ Assets/HackerM4ge/Scripts/RippleIn.cs
@@ -7,12 +7,15 @@ public class RippleIn : MonoBehaviour {
     public float minDistance = 2f;
     public bool showing = false;
     public float delay = 1f;
+    public int randomCount = 4;
 
     protected Animator[] children;
+    protected bool[] highlighted;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         children = GetComponentsInChildren<Animator>();
+        highlighted = new bool[children.Length];
         for (int i = 0; i < children.Length; i++)
         {
             children[i].SetBool("Shown", showing);
@@ -25,24 +28,42 @@ public class RippleIn : MonoBehaviour {
         if (delta.magnitude < minDistance)
         {
             if (showing) return;
-            showing = true;
+            RandomizeSelectedChildren();
             StartCoroutine("ActivateRipple", true);
         }
         else
         {
             if (!showing) return;
-            showing = false;
             StartCoroutine("ActivateRipple", false);
+        }
+    }
+
+    private void RandomizeSelectedChildren() {
+        for(int i = 0; i < children.Length; i++)
+        {
+            highlighted[i] = false;
+        }
+        int randomChildren = 0;
+        while(randomChildren < randomCount)
+        {
+            highlighted[Random.Range(0, children.Length)] = true;
+            randomChildren = 0;
+            for (int i = 0; i < children.Length; i++)
+            {
+                if (highlighted[i]) randomChildren++;
+            }
         }
     }
 
     public IEnumerator ActivateRipple(bool state)
     {
+        showing = state;
         yield return new WaitForSeconds(delay);
 
         for (int i = 0; i < children.Length; i++)
         {
             children[i].SetBool("Shown", state);
+            if (!state || highlighted[i]) children[i].SetBool("Highlighted", state);
             yield return new WaitForSeconds(delay);
         }
     }
