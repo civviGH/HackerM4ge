7bf363753328e349015eb269dc63766027c70a47
Lars Wallenborn <lars@wallenborn.net>
2017-08-26 18:19:07 +0200

    starst with health and mana bar

---
+++ Assets/ShowHealthAndMana.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowHealthAndMana : MonoBehaviour {
+    protected Animator[] children;
+
+	// Use this for initialization
+	void Start ()
+    {
+        children = GetComponentsInChildren<Animator>();
+        highlighted = new bool[children.Length];
+        for (int i = 0; i < children.Length; i++)
+        {
+            children[i].SetBool("Shown", false);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
